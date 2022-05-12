238616199121ed4e0568000a0a451aec05360373
Chaithanya H P <92391607+Chai-3@users.noreply.github.com>
2022-05-12 16:12:14 +0530

    Add files via upload

---
+++ SQLiteDb.java
@@ -0,0 +1,37 @@
+package net.codejava;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class SQLiteDb {
+
+	public static void main(String[] args) {
+
+		String jdbcUrl="jdbc:sqlite:/C:\\sqlite\\sqlite-tools-win32-x86-3380500\\mydb.db";
+		try
+		{
+			Connection connection=DriverManager.getConnection(jdbcUrl);
+			String sql="SELECT * FROM movie";
+			Statement statement=connection.createStatement();
+			ResultSet result = statement.executeQuery(sql);
+			
+			while (result.next()) {
+				String name=result.getString("moviename");
+				String actor=result.getString("actor");
+				String actress=result.getString("actress");
+				String year=result.getString("year");
+				String directorname=result.getString("directorname");
+				
+				System.out.println(name +"|"+ actor +"|"+ actress +"|"+ year +"|" + directorname );	
+			}
+		} catch(SQLException e) {
+			System.out.println("Error connecting to SQLite database");
+			e.printStackTrace();
+			
+		}
+	}
+
+}
